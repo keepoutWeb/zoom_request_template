896dda326e1c590f1f57b201da3dbf6da99639cb
rjaylamadrid <rjay.lamadrid@cbsua.edu.ph>
2021-09-16 15:18:48 +0800

    update

--- inc/config.php
+++ inc/config.php
@@ -19,3 +19,22 @@ if (isset ($_SESSION['user_profile'])) {
     $user_profile = $_SESSION['user_profile'];
     $smarty->assign ('user_profile', $user_profile);
 }
+
+// DB CONFIG
+$host = 'localhost';
+$db   = 'db_cb';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+try {
+     $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
